ef918d564977974bf45c4cf8e66e5d4bf614c4f5
Stephen Wang <35492791+swang99@users.noreply.github.com>
2021-02-06 00:10:47 -0500

    Update tictactoe.py

--- tictactoe/tictactoe.py
+++ tictactoe/tictactoe.py
@@ -9,21 +9,12 @@ X = "X"
 O = "O"
 EMPTY = None
 
-
-def initial_state():
-    # Returns starting state of the board.
-    return [[EMPTY, EMPTY, EMPTY],
-            [EMPTY, EMPTY, EMPTY],
-            [EMPTY, EMPTY, EMPTY]]
-
+def initial_state(): # starting 3x3 board
+    return [[EMPTY, EMPTY, EMPTY] for i in range(3)]
 
 def player(board):
     # Returns player who has the next turn on a board.
-    if sum(x.count("X") for x in board) == sum(x.count("O") for x in board):
-        return X
-    elif sum(x.count("X") for x in board) > sum(x.count("O") for x in board):
-        return O
-
+    return X if sum(x.count("X") for x in board) == sum(x.count("O") for x in board) else O
 
 def actions(board):
     # Returns set of all possible actions (i, j) available on the board.
@@ -34,7 +25,6 @@ def actions(board):
                 action.append((i, j))
     return action
 
-
 def result(board, action):
     # Returns the board that results from making move (i, j) on the board.
     board = copy.deepcopy(board)
@@ -42,13 +32,10 @@ def result(board, action):
     if board[i][j] == EMPTY:
         board[i][j] = player(board)
         return board
-    else:
-        raise Exception("Invalid move")
-
+    raise Exception("Invalid move")
 
 def winner(board):
     # Returns the winner of the game, if there is one.
-    # Win scenarios for X and O: 3 horizontals, 3 verticals, 2 diagonals
     # For X
     if board[0] == ["X", "X", "X"] or board[1] == ["X", "X", "X"] or board[2] == ["X", "X", "X"]\
             or (board[0][0] == "X" and board[1][0] == "X" and board[2][0] == "X")\
@@ -57,72 +44,57 @@ def winner(board):
             or (board[0][0] == "X" and board[1][1] == "X" and board[2][2] == "X")\
             or (board[0][2] == "X" and board[1][1] == "X" and board[2][0] == "X"):
         return X
+
     # For O
-    elif board[0] == ["O", "O", "O"] or board[1] == ["O", "O", "O"] or board[2] == ["O", "O", "O"]\
+    if board[0] == ["O", "O", "O"] or board[1] == ["O", "O", "O"] or board[2] == ["O", "O", "O"]\
             or (board[0][0] == "O" and board[1][0] == "O" and board[2][0] == "O")\
             or (board[0][1] == "O" and board[1][1] == "O" and board[2][1] == "O")\
             or (board[0][2] == "O" and board[1][2] == "O" and board[2][2] == "O")\
             or (board[0][0] == "O" and board[1][1] == "O" and board[2][2] == "O")\
             or (board[0][2] == "O" and board[1][1] == "O" and board[2][0] == "O"):
         return O
-    # In progress or tie
-    else:
-        return None
-
 
 def terminal(board):
-    # Returns True if game is over, False otherwise.
-    if sum(x.count(EMPTY) for x in board) == 0:
-        return True
-    elif winner(board) is not None:
-        return True
-    else:
-        return False
-    
+    # Is the game over?
+    return True if sum(x.count(EMPTY) for x in board) == 0 or winner(board) else False
 
 def utility(board):
     # Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
     if winner(board) == X:
         return 1
-    elif winner(board) == O:
+    if winner(board) == O:
         return -1
-    else:
-        return 0
-
+    return 0
 
 def minimax(board):
     if player(board) == X:
         v = -math.inf
         for action in actions(board):
-            v_op = minvalue(result(board, action)) 
+            v_op = minVal(result(board, action)) 
             if v_op > v:
                 v = v_op
                 optimal_move = action
     else:
         v = math.inf
         for action in actions(board):
-            v_op = maxvalue(result(board, action)) 
+            v_op = maxVal(result(board, action)) 
             if v_op < v:
                 v = v_op
                 optimal_move = action
     return optimal_move
 
-
-def maxvalue(board):
-    # Maximizing function for X
+def maxVal(board): # Maximizing function for X
     if terminal(board):
         return utility(board)
     v = -math.inf
     for action in actions(board):
-        v = max(v, minvalue(result(board, action)))
+        v = max(v, minVal(result(board, action)))
     return v    
 
-
-def minvalue(board):
-    # Minimizing function for O
+def minVal(board): # Minimizing function for O
     if terminal(board):
         return utility(board)
     v = math.inf
     for action in actions(board):
-        v = min(v, maxvalue(result(board, action)))
-    return v    
+        v = min(v, maxVal(result(board, action)))
+    return v  
